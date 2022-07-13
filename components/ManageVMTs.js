5311f4c0c0d67aae383b551effb667a31e324a58
its94 <103844813+its94@users.noreply.github.com>
2022-07-13 18:48:16 +0300

    vsa

--- components/ManageVMTs.js
+++ components/ManageVMTs.js
@@ -17,7 +17,7 @@ const ManageVMTs = (props) => {
     <Box paddingLeft="7%" paddingRight="7%">
       <Flex>
         <Text fontSize="xl" ml="2em" fontWeight="bold">
-          Staked VRMC 'List'
+          Manage VMTs
         </Text>
       </Flex>
 
@@ -43,5 +43,120 @@ const ManageVMTs = (props) => {
           </Flex>
         </Flex>
 
-       }
+        <Box>
+          <SimpleGrid
+            columns={25}
+            spacing={0.5}
+            fontSize="xx-small"
+            color="gray.400"
+          >
+            <Text bg="#fff"></Text>
+            <Text bg="#fff">STATUS</Text>
+            <Text bg="#fff"></Text>
+            <Text bg="#fff"></Text>
+            <Text bg="#fff"></Text>
+            <Text bg="#fff">ADDRESS</Text>
+            <Text bg="#fff"></Text>
+            <Text bg="#fff"></Text>
+            <Text bg="#fff"></Text>
+            <Text bg="#fff"></Text>
+            <Text bg="#fff"></Text>
+            <Text bg="#fff"></Text>
+            <Text bg="#fff">NAME</Text>
+            <Text bg="#fff"></Text>
+            <Text bg="#fff"></Text>
+            <Text bg="#fff">BALANCE</Text>
+            <Text bg="#fff"></Text>
+            <Text bg="#fff"></Text>
+            <Text bg="#fff">USAGE</Text>
+            <Text bg="#fff"></Text>
+            <Text bg="#fff">CREATED</Text>
+            <Text bg="#fff"></Text>
+            <Text bg="#fff"></Text>
+            <Text bg="#fff"></Text>
+            <Text bg="#fff"></Text>
+          </SimpleGrid>
+
+          <Flex
+            justifyContent="space-between"
+            backgroundColor="#fff"
+            borderRadius={155}
+            boxShadow="md"
+            padding="1em"
+            pl="5em"
+            pr="5em"
+            fontSize="smaller"
+            color="gray.600"
+            margin="1em"
+          >
+            <Text>
+              <Image src="enabled_vmt.svg" alt="enabled" />
+            </Text>
+            <Text>0xdAC17F958D2ee523a2206206994597C13D831ec7</Text>
+            <Text>Test Merkle Tree #1</Text>
+            <Text>28.7502 LINK</Text>
+            <Text isNumeric>72</Text>
+            <Text>10.05.2022</Text>
+            <Text>
+              <Link href="#">
+                <SettingsIcon />
+              </Link>
+            </Text>
+          </Flex>
+          <Flex
+            justifyContent="space-between"
+            backgroundColor="#fff"
+            borderRadius={155}
+            boxShadow="md"
+            padding="1em"
+            pl="5em"
+            pr="5em"
+            fontSize="smaller"
+            color="gray.600"
+            margin="1em"
+          >
+            <Image src="disabled_vmt.svg" alt="disabled" />
+            <Text>0xdAC17F958D2ee523a2206206994597C13D831ec7</Text>
+            <Text>Test Merkle Tree #1</Text>
+            <Text>28.7502 LINK</Text>
+            <Text isNumeric>72</Text>
+            <Text>10.05.2022</Text>
+            <Text>
+              <Link href="#">
+                <SettingsIcon />
+              </Link>
+            </Text>
+          </Flex>
+          <Flex
+            justifyContent="space-between"
+            backgroundColor="#fff"
+            borderRadius={155}
+            boxShadow="md"
+            padding="1em"
+            pl="5em"
+            pr="5em"
+            fontSize="smaller"
+            color="gray.600"
+            margin="1em"
+          >
+            <Text>
+              <Image src="enabled_vmt.svg" alt="enabled" />
+            </Text>
+            <Text>0xdAC17F958D2ee523a2206206994597C13D831ec7</Text>
+            <Text>Test Merkle Tree #1</Text>
+            <Text>28.7502 LINK</Text>
+            <Text isNumeric>72</Text>
+            <Text>10.05.2022</Text>
+            <Text>
+              <Link href="#">
+                <SettingsIcon />
+              </Link>
+            </Text>
+          </Flex>
+        </Box>
+      </Box>
+    </Box>
+  );
+};
+
 export default ManageVMTs;
